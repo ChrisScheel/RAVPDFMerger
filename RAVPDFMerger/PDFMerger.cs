d362720966e8c710c59a217a1871b4d72f5cc910
Christian Scheel <christian.scheel@bluewin.ch>
2021-07-01 20:09:40 +0200

    Add first commit

---
+++ RAVPDFMerger/PDFMerger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RAVPDFMerger
+{
+    class PDFMerger
+    {
+        //make this a super or even an abstract class for general pdf merging
+        //create a new one for the specific rav pdf merging
+    }
+}
